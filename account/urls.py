2ecdb52f297de11c0d16a1ef89cd4cc5e9a41c96
Nilsolk <awer258@gmail.com>
2025-02-25 23:03:06 +0300

    Login logout mainpage

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login)
+    path('login/', auth_views.LoginView.as_view(), name = 'login_view'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout_view'),
+    path('', views.dashboard, name = 'board')
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == "POST":
@@ -20,3 +21,8 @@ def user_login(request):
         form = LoginForm()
     context = {'form': form}
     return render(request, 'login.html', context)
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'board.html')
